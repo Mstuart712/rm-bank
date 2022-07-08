31bfae4f51b7592d7ca140483a44df9b6a655fd8
Mstuart712 <stuartconcept@gmail.com>
2022-07-08 12:59:17 -0600

    starting over for now

--- main.go
+++ main.go
@@ -1,17 +1,7 @@
 package main
 
-import (
-	"os"
-
-	"github.com/Mstuart712/rm-bank/internal/cmd"
-)
-
 var version = "dev"
 
 func main() {
-	cmd.Execute(
-		version,
-		os.Exit,
-		os.Args[1:],
-	)
+
 }
